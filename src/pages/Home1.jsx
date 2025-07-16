aa431c0b5759837e4efac5a6e1d14287cfeb0340
Alexander <alexander.jbc28@gmail.com>
2025-07-16 00:32:43 -0600

    seccion de blog

--- src/pages/Home1.jsx
+++ src/pages/Home1.jsx
@@ -2,7 +2,8 @@ import { Navbar } from '../components/Navbar';
 import { Hero } from '../components/Hero';
 import { Mision } from '../components/Mision';
 import ContactForm from '/src/components/Formulario.jsx';
-
+import  Footer  from '../components/Footer';
+import  Blog  from '../components/Blog';
 
 export const Home1 = () => {
   return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -12,9 +13,11 @@ export const Home1 = () => {
     <main>
       <Hero />
       <Mision />
+      <Blog />
       <ContactForm />
 
     </main>
+    <Footer />
     {/*Footer*/}
     </div>;
 }
